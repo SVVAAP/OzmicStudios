7df5f420248ec9df76099082665f09bfb58ad7ec
SVVAAP <workwithsvvaap@gmail.com>
2023-09-28 12:30:55 +0530

    hii

--- firebase.js
+++ firebase.js
@@ -7,15 +7,17 @@
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyB8BFWhzzmsL-xAr9sK_TmXLdlE4q1qaR0",
-    authDomain: "ozmic-web-9ed4b.firebaseapp.com",
-    projectId: "ozmic-web-9ed4b",
-    storageBucket: "ozmic-web-9ed4b.appspot.com",
-    messagingSenderId: "529520113248",
-    appId: "1:529520113248:web:4ad46077cb041423c83505",
-    measurementId: "G-B91Y2PP4MC"
-  };
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB8BFWhzzmsL-xAr9sK_TmXLdlE4q1qaR0",
+  authDomain: "ozmic-web-9ed4b.firebaseapp.com",
+  databaseURL: "https://ozmic-web-9ed4b-default-rtdb.firebaseio.com",
+  projectId: "ozmic-web-9ed4b",
+  storageBucket: "ozmic-web-9ed4b.appspot.com",
+  messagingSenderId: "529520113248",
+  appId: "1:529520113248:web:4ad46077cb041423c83505",
+  measurementId: "G-B91Y2PP4MC"
+};
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
